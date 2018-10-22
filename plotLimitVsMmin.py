ba636541661f3ff1f254541fedd012a20a4ab098
Fan Zhang <zhan2600@purdue.edu>
2018-10-21 23:25:53 -0400

    plot limit vs Mmin .py + .png

---
+++ plotLimitVsMmin.py
@@ -0,0 +1,143 @@
+import ROOT
+from ROOT import TFile, TTree, TCanvas, TGraph, TMultiGraph, TGraphErrors, TLegend
+import CMS_lumi, tdrstyle
+import subprocess # to execute shell command
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+ 
+# CMS style
+CMS_lumi.cmsText = "CMS"
+CMS_lumi.extraText = "Preliminary"
+CMS_lumi.cmsTextSize = 0.65
+CMS_lumi.outOfFrame = True
+tdrstyle.setTDRStyle()
+ 
+# scale by xsec or not
+XSec = False
+ 
+# GET limits from root file
+def getLimits(file_name):
+ 
+    file = TFile(file_name)
+    tree = file.Get("limit")
+ 
+    limits = [ ]
+    for quantile in tree:
+        limits.append(tree.limit)
+        # print ">>>   %.2f" % limits[-1]
+ 
+    return limits[:6]
+ 
+ 
+# calculate limit +- 1,2 sigma
+def getCross(Mmin, label):
+ 
+    lambdas = [6000, 7000]
+    limit1 = getLimits("./dataCards/ee_limit_min%d_%s/higgsCombineTest.AsymptoticLimits.mH%d.root"%(Mmin, label, 6000))
+    limit2 = getLimits("./dataCards/ee_limit_min%d_%s/higgsCombineTest.AsymptoticLimits.mH%d.root"%(Mmin, label, 7000))
+    cross = [0.0]*5
+    for j in range(5):
+        # y1 = a x1 + b, y2 = a x2 + b
+        a = (limit2[j] - limit1[j]) / 1.0
+        b = limit2[j] - a * 7
+        cross[j] = (1 - b) / a
+
+    return cross
+
+
+# plot limit vs Mmin
+def plotLimits(label):
+
+    Mmin = [2600, 2700, 2800, 2900, 3000, 3100, 3200]
+    N = len(Mmin)
+    yellow = TGraph(2*N)    # yellow band
+    green = TGraph(2*N)     # green band
+    median = TGraph(N)      # median
+    
+    for i in range(N):
+        limit = getCross(Mmin[i], label)
+        yellow.SetPoint(    i,    Mmin[i], limit[4]) # + 2 sigma
+        green.SetPoint(     i,    Mmin[i], limit[3]) # + 1 sigma
+        median.SetPoint(    i,    Mmin[i], limit[2]) # median
+        green.SetPoint(  2*N-1-i, Mmin[i], limit[1]) # - 1 sigma
+        yellow.SetPoint( 2*N-1-i, Mmin[i], limit[0]) # - 2 sigma
+    
+    W = 800
+    H  = 600
+    T = 0.08*H
+    B = 0.12*H
+    L = 0.12*W
+    R = 0.04*W
+    c = TCanvas("c","c",100,100,W,H)
+    c.SetFillColor(0)
+    c.SetBorderMode(0)
+    c.SetFrameFillStyle(0)
+    c.SetFrameBorderMode(0)
+    c.SetLeftMargin( L/W )
+    c.SetRightMargin( R/W )
+    c.SetTopMargin( T/H )
+    c.SetBottomMargin( B/H )
+    c.SetTickx(0)
+    c.SetTicky(0)
+    c.SetGrid()
+    c.cd()
+    frame = c.DrawFrame(2600, 5.5, 3200, 8.5)
+    frame.GetYaxis().CenterTitle()
+    frame.GetYaxis().SetTitleSize(0.05)
+    frame.GetXaxis().SetTitleSize(0.05)
+    frame.GetXaxis().SetLabelSize(0.04)
+    frame.GetYaxis().SetLabelSize(0.04)
+    frame.GetYaxis().SetTitleOffset(0.9)
+    frame.GetXaxis().SetNdivisions(508)
+    frame.GetYaxis().CenterTitle(True)
+    frame.GetYaxis().SetTitle("95% upper limit on #sigma / #sigma_{SM}")
+    #frame.GetYaxis().SetTitle("95% upper limit on #sigma #times BR / (#sigma #times BR)_{SM}")
+    frame.GetXaxis().SetTitle("#M_{min} (GeV)")
+    #frame.SetMinimum(0)
+    #frame.SetMaximum(max(up2s)*1.05)
+    #frame.GetXaxis().SetLimits(min(values),max(values))
+ 
+    yellow.SetFillColor(ROOT.kOrange)
+    yellow.SetLineColor(ROOT.kOrange)
+    yellow.SetFillStyle(1001)
+    yellow.Draw('F')
+ 
+    green.SetFillColor(ROOT.kGreen+1)
+    green.SetLineColor(ROOT.kGreen+1)
+    green.SetFillStyle(1001)
+    green.Draw('Fsame')
+ 
+    median.SetLineColor(1)
+    median.SetLineWidth(2)
+    median.SetLineStyle(2)
+    median.Draw('Lsame')
+ 
+    CMS_lumi.CMS_lumi(c,13,11)
+    ROOT.gPad.SetTicks(1,1)
+    frame.Draw('sameaxis')
+ 
+    x1 = 0.15
+    x2 = x1 + 0.24
+    y2 = 0.76
+    y1 = 0.60
+    legend = TLegend(x1,y1,x2,y2)
+    legend.SetFillStyle(0)
+    legend.SetBorderSize(0)
+    legend.SetTextSize(0.041)
+    legend.SetTextFont(42)
+    legend.AddEntry(median, "Asymptotic CL_{s} expected %s"%label,'L')
+    legend.AddEntry(green, "#pm 1 std. deviation",'f')
+    legend.AddEntry(yellow,"#pm 2 std. deviation",'f')
+    legend.Draw()
+
+    c.SaveAs("limits/Limit_ee_LimitVsMmin_%s.png"%label)
+    c.Close()
+
+ 
+# MAIN
+def main():
+    plotLimits("Con")
+    plotLimits("Des")
+ 
+ 
+if __name__ == '__main__':
+    main()
